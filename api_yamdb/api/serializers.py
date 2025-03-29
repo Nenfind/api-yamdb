89cc354093e9c62e360dc796425cabf2b77589f8
Alexander Kuznetsov <alex.raibon@yandex.ru>
2025-03-29 10:09:41 +0300

    удалил validate_score и поправил from api.models

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -35,10 +35,6 @@ class ReviewsSerializer(serializers.ModelSerializer):
             'pub_date',
         )
 
-    def validate_score(self, value):
-        if value < 1 or value > 10:
-            raise serializers.ValidationError('Оценка должна быть от 1 до 10.')
-        return value
 
     def validate(self, data):
         """
