07324166e4b4a3705eb7c338fd38abf78eb191be
Di Na Pak <modest.saz@gmail.com>
2025-03-29 12:50:49 +0300

    Revert "Fix/nen"

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,9 +6,11 @@ from reviews.models import Title
 
 class TitleFilter(django_filters.FilterSet):
     """Фильтр произведений по жанрам, категориям и другим полям."""
+    id = django_filters.NumberFilter(field_name='id')
     genre = django_filters.CharFilter(field_name='genre__slug')
     category = django_filters.CharFilter(field_name='category__slug')
     name = django_filters.CharFilter(lookup_expr='icontains')
+    year = django_filters.NumberFilter()
 
     class Meta:
         """Мета-класс для фильтра."""
